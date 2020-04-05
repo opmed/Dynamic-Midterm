611dcdd46bf9d450a5d2749ed7637554cde63a6c
Ahmed_Elhussiny <ae1561@nyu.edu>
2020-04-05 00:30:02 -0400

    final commit

--- App.js
+++ App.js
@@ -1,28 +1,78 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import "./App.css"
-import news from '../container/news';
-import quotes from '../container/quotes';
+import Stock from './Container/Stock';
+import './App.css';
+
+import axios from "axios";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/" component={news} />
-        <Route exact path="/search" component={quotes} />
-      </Router>
+      <Stock></Stock>
     </div>
   );
 }
 
 
-/*Change background color*/
+
 function changeBackground(color) {
    document.body.style.background = color;
 }
-window.addEventListener("load",function() { changeBackground('lightblue') });
+window.addEventListener("load",function() { changeBackground('#122') });
 
+
+
+/*---------------------------------------------------------------------*/
+/*class News extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+    }
+
+
+
+    componentDidMount() {
+          fetch('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e10eca7e6be44f1ba17d9804bec868d3') 
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+
+    render() {
+        const { isLoaded, items } = this.state;
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+        <div className="boxes">  
+          <button>
+            <div className="App">
+                <ul>
+                    {items.map(item => (  
+                        <li key={item.id}>   
+                             {item.name} - {item.email}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </button>
+      </div>
+
+        );
+    }
+}*/
 
 
 export default App;
---
+++ Container/Stock.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+import axios from "axios";
+
+
+
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchStock();
+  }
+
+  fetchStock() {
+    const pointerToThis = this;
+    let StockSymbol = 'UAL';
+    const API_KEY = 'HGJWFG4N8AQ66ICD';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
+
+    fetch(API_Call)
+      .then(
+        function(response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data) {
+
+          for (var key in data['Time Series (Daily)']) {
+            stockChartXValuesFunction.push(key);
+            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+          }
+
+          pointerToThis.setState({
+            stockChartXValues: stockChartXValuesFunction,
+            stockChartYValues: stockChartYValuesFunction
+          });
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div>
+        <center><h1>United Airlines Quotes</h1></center>
+        <Plot
+          data={[
+            {
+              x: this.state.stockChartXValues,
+              y: this.state.stockChartYValues,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            }
+          ]}
+          layout={{width: 820, height: 440, title: 'UAL'}}
+        />
+      </div>
+    )
+  }
+}
+
+export default Stock;
+
+
